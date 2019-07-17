40eac451e222a26e5f8eeae1687fc665e587b96e
Ralph Comanne <ralph@comanne.eu>
2019-07-17 19:00:42 +0200

    removed old name

--- scripts/copy_and_start.sh
+++ scripts/copy_and_start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "removing old jar"
-rm /var/opt/services/telegram-bot-klootviool/telegram-bot-klootviool-service-0.0.1-SNAPSHOT.jar
+rm /var/opt/services/telegram-bot-klootviool/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar
 
 echo "copying new jar to /var/opt/services/opt/services/telegram-bot-klootviool"
-cp target/telegram-bot-klootviool-service-0.0.1-SNAPSHOT.jar /var/opt/services/telegram-bot-klootviool/
+cp target/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar /var/opt/services/telegram-bot-klootviool/
 
 echo "restarting the service"
 sudo systemctl restart telegram-bot-klootviool.service
