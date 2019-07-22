821a9e8c07ee32eeac5f60b140d6fc4c25a3a0f6
Ralph Comanne <ralph.comanne@rabobank.nl>
2019-07-22 11:09:30 +0200

    now copying logback file to server runtime enabled additivity, max history for rolling is now 7

--- scripts/copy_and_start.sh
+++ scripts/copy_and_start.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+BASE_DIR=/var/opt/services/telegram-bot-klootviool
+CONF_DIR=$(BASE_DIR)/config
+
+echo "removing old logback file"
+rm $(CONF_DIR)/logback.xml
+
+echo "copying new logback file"
+cp target/logback-server.xml $(CONF_DIR)/logback.xml
+
 echo "removing old jar"
-rm /var/opt/services/telegram-bot-klootviool/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar
+rm $(BASE_URL)/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar
 
 echo "ensure jar is executable"
 chmod +x target/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar
 
 echo "copying new jar to /var/opt/services/opt/services/telegram-bot-klootviool"
-cp target/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar /var/opt/services/telegram-bot-klootviool/
+cp target/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar $(BASE_URL)/
 
 echo "restarting the service"
 sudo systemctl restart telegram-bot-klootviool.service
