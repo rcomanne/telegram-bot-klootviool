385272d283d0701871cdcc08f22ea82d0b8bc01e
Ralph Comanne <ralph.comanne@rabobank.nl>
2019-08-28 09:32:14 +0200

    made loop with wait for deploy message

--- scripts/copy_and_start.sh
+++ scripts/copy_and_start.sh
@@ -21,4 +21,14 @@ cp target/telegram-bot-klootviool-0.0.1-SNAPSHOT.jar ${BASE_DIR}/
 echo "restarting the service"
 sudo systemctl restart telegram-bot-klootviool.service
 
-curl -XPOST 'localhost:8101/message/-320932775/deployed_new_version'
+echo "sleeping and then trying to send deploy message from the service"
+ret=0
+i=1
+while [ ${i} -le 10 -a "${ret}" -ne "200" ]
+do
+	echo "waiting for 15 secs"
+	sleep 15
+	ret=`curl -s -o /dev/null -w "%{http_code}" -XPOST http://localhost:8101/message/-320932775/deployed_new_version`
+	echo "return code: ${ret}"
+	let i=${i}+1
+done
