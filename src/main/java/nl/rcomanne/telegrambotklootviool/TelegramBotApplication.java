1f67b3433833018e28ef4af2aadce4448e8d1092
Ralph Comanne <ralph.comanne@rabobank.nl>
2019-08-28 09:42:12 +0200

    added cron job

--- src/main/java/nl/rcomanne/telegrambotklootviool/TelegramBotApplication.java
+++ src/main/java/nl/rcomanne/telegrambotklootviool/TelegramBotApplication.java
@@ -2,8 +2,10 @@ package nl.rcomanne.telegrambotklootviool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
+@EnableScheduling
 @SpringBootApplication
 public class TelegramBotApplication {
     public static void main(String[] args) {
---
+++ src/main/java/nl/rcomanne/telegrambotklootviool/scheduled/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package nl.rcomanne.telegrambotklootviool.scheduled;
+
+import nl.rcomanne.telegrambotklootviool.service.MessageService;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduledTasks {
+    private static final String CHAT_ID = "-320932775";
+
+    private final MessageService messageService;
+
+    @Scheduled(cron = "*/15 * * * * *")
+    public void sendDailyMessage() {
+        messageService.sendMessage(CHAT_ID, "goedemorgen thirsty bois");
+    }
+}
