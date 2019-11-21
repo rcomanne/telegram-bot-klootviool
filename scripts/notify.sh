d2e29719af82f47f24a43ede58c3192cc9ed8a53
Ralph Comanne <ralph@comanne.eu>
2019-11-21 20:48:35 +0000

    send notification to myself

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -7,7 +7,7 @@ while [ ${i} -le 10 -a "${ret}" -ne "200" ]
 do
 	echo "waiting for 15 secs"
 	sleep 15
-	ret=`curl -s -o /dev/null -w "%{http_code}" -XPOST http://localhost:8101/message/-320932775/deployed_new_version`
+	ret=`curl -s -o /dev/null -w "%{http_code}" -XPOST http://localhost:8101/message/620393195/deployed_new_version`
 	echo "return code: ${ret}"
 	let i=${i}+1
 done
